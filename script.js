9133573b6cf54f73b6fc07008de9a0e56094fff3
Harsh Shah <harsh-bhavesh.shah@etu.univ-amu.fr>
2025-12-20 11:33:18 +0100

    Remove scroll event listener for header

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const header = document.getElementById('glass-header');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
