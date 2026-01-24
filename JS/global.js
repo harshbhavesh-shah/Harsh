63506462e34052f19dbb2123b33c55ddab13309f
Harsh Shah <harsh-bhavesh.shah@etu.univ-amu.fr>
2026-01-24 12:04:24 +0100

    Clean up comments in global.js
    Removed unnecessary comment about scroll event listener.

--- JS/global.js
+++ JS/global.js
@@ -1,6 +1,4 @@
 const header = document.getElementById('main-header');
-
-        // Listen for the scroll event
         window.addEventListener('scroll', () => {
             // Check if user has scrolled more than 50 pixels down
             if (window.scrollY > 50) {
